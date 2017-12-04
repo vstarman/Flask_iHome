175e9070125d9f3a0f7452629accc46d39894fa6
vstarman <vstarman@foxmail.com>
2017-12-04 10:08:21 +0800

    Add redis config

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
 
 
 class Config(object):
@@ -11,6 +12,9 @@ class Config(object):
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost:3306/Flask_iHome'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis配饰
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 
 app = Flask(__name__)
@@ -18,10 +22,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # 初始化数据库
 db = SQLAlchemy(app)
+# 初始化redis
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/index')
 def index():
+    # redis_store.set('name', 'xiaofang')
     return 'index'
 
 if __name__ == '__main__':
