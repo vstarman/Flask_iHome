0af428eba73d33802aeea2e7fb394b2f311cd536
vstarman <vstarman@foxmail.com>
2017-12-06 17:22:46 +0800

    add loglog: current_app.logger.debug

--- iHome/__init__.py
+++ iHome/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 import redis
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
@@ -13,6 +15,17 @@ db = SQLAlchemy()
 redis_store = None
 # 配置csrf,校验表单,防止跨站请求伪造
 csrf = CSRFProtect()
+
+# 设置日志的记录等级
+logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+# 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象（flask app使用的）添加日志记录器
+logging.getLogger().addHandler(file_log_handler)
 
 
 def create_app(config_name):
@@ -32,7 +45,6 @@ def create_app(config_name):
     Session(app)
     # 配置csrf,校验表单,防止跨站请求伪造
     csrf.init_app(app)
-
     # 注册蓝图
     from iHome import api_1_0
     app.register_blueprint(api_1_0.api, url_prefix='/api/v1.0')
--- iHome/api_1_0/index.py
+++ iHome/api_1_0/index.py
@@ -1,4 +1,7 @@
-from flask import session
+# -*- coding:utf-8 -*-
+
+import logging
+from flask import session, current_app
 from . import api
 from iHome import redis_store
 
@@ -7,4 +10,9 @@ from iHome import redis_store
 def index():
     session['name'] = 'xiaohua'
     redis_store.set('name', 'xiaoli')
+    # 其他项目使用
+    logging.debug('DEBUG LOGLOG')
+    print '='*50
+    # flask内置,信息更详细
+    current_app.logger.debug('LOG LOG CURRENT')
     return 'index'
