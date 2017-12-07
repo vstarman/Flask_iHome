d8bee404675cbb3921e8fc3d198c515cb0beb95b
vstarman <vstarman@foxmail.com>
2017-12-07 17:11:51 +0800

    Add third-party packages 'yuntongxun' to send short message

--- iHome/api_1_0/verify.py
+++ iHome/api_1_0/verify.py
@@ -6,21 +6,26 @@ from iHome.utils.captcha.captcha import captcha
 from iHome import redis_store, constants
 from iHome.utils.response_code import RET
 
+#
+# @api.route('/sms', method=['POST'])
+# def send_sms():
+#     """:发送短信验证码
+#     1.获取参数并判断是否为空
+#     2.判断手机号是否合法
+#     3.取到redis总缓存的验证码内容,校验验证码
+#     4.生成短信验证码
+#     5.给前端响应结果
+#     :return:
+#     """
+    # 1.获取参数并判断是否为空
+    # data =
 
-@api.route('/sms', method=['POST'])
-def send_sms():
-    """
-    1.获取参数并判断是否为空
-    2.判断手机号是否合法
-    3.取到redis总缓存的验证码内容
-    4.校验验证码,如验证
-    :return:
-    """
+
 
 
 @api.route('/imagecode')
 def get_image_code():
-    """
+    """:发送图片验证码
     1.取到传入图片编码
     2.生成图片验证码
     3.存储到redis中(key是图片编码,值是验证码文字内容)
@@ -38,14 +43,12 @@ def get_image_code():
 
     # 2.生成验证码
     _, text, image = captcha.generate_captcha()
-    # 测试: 终端打印text
-    # current_app.logger.debug(text)
 
     # 3.存储到redis中(key是图片编码,值是图片text内容)
     # redis_store.set('key', 'value', '过期时间')
     try:
         if pre:
-            current_app.logger.debug(pre)
+            # current_app.logger.debug(pre)
             redis_store.delete('ImageCode_'+pre)
         redis_store.set('ImageCode_'+cur, text, constants.IMAGE_CODE_REDIS_EXPIRES)
     except Exception as e:
--- iHome/utils/SMS.py
+++ iHome/utils/SMS.py
@@ -28,32 +28,53 @@ softVersion = '2013-12-26'
 
 class CCP(object):
     """发送模板短信"""
-    # def __init__(cls):
-    #     if
+    def __new__(cls, *args, **kwargs):
+        """用单例只初始化一次SDK"""
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(CCP, cls).__new__(cls, args, kwargs)
+            cls._instance.rest = REST(serverIP, serverPort, softVersion)
+            cls._instance.rest.setAccount(accountSid, accountToken)
+            cls._instance.rest.setAppId(appId)
+        return cls._instance
 
-
-
-def sendTemplateSMS(to, datas, tempId):
-    """
-    发送模板短信
-    :param to: 手机号码
-    :param datas: 内容数据 格式为数组 例如：{'12', '34'}，如不需替换请填 ''
-    :param tempId: 模板Id
-    :return:
-    """
-    # 初始化REST SDK
-    rest = REST(serverIP, serverPort, softVersion)
-    rest.setAccount(accountSid, accountToken)
-    rest.setAppId(appId)
-    
-    result = rest.sendTemplateSMS(to, datas, tempId)
-    for k, v in result.iteritems(): 
-        
-        if k == 'templateSMS':
-                for j, s in v.iteritems(): 
-                    print '%s:%s' % (j, s)
+    def send_text_sms(self, to, data, temp_id):
+        """
+        发送模板短信
+        :param to: 手机号码
+        :param data: 内容数据 格式为数组 例如：{'12', '34'}，如不需替换请填 ''
+        :param temp_id: 模板Id
+        :return:
+        """
+        result = self.rest.sendTemplateSMS(to, data, temp_id)
+        if result.get('statusCode') == '000000':
+            return 1    # 表示发送成功
         else:
-            print '%s:%s' % (k, v)
-    
-   
-sendTemplateSMS(18832667432, ['12', '34'], '1')
+            return 0    # 表示发送失败
+
+
+# def sendTemplateSMS(to, datas, tempId):
+#     """
+#     发送模板短信
+#     :param to: 手机号码
+#     :param datas: 内容数据 格式为数组 例如：{'12', '34'}，如不需替换请填 ''
+#     :param tempId: 模板Id
+#     :return:
+#     """
+#     # 初始化REST SDK
+#     rest = REST(serverIP, serverPort, softVersion)
+#     rest.setAccount(accountSid, accountToken)
+#     rest.setAppId(appId)
+#
+#     result = rest.sendTemplateSMS(to, datas, tempId)
+#     for k, v in result.iteritems():
+#
+#         if k == 'templateSMS':
+#                 for j, s in v.iteritems():
+#                     print '%s:%s' % (j, s)
+#         else:
+#             print '%s:%s' % (k, v)
+
+
+if __name__ == '__main__':
+    CCP().send_text_sms('18832667432', ['888888', 100], 1)
+    # sendTemplateSMS('18832667432', ['888888', 100], 1)
